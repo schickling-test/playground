6085cbcf92f0aef78cd15c11e895099db6b26265
Johannes Schickling <schickling.j@gmail.com>
2021-11-03 09:52:43 +0000

    WIP repro

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import '@effect-ts/core/Tracing/Enable'
-import { Effect as T, pipe } from '@effect-ts/core'
+import { Effect as T, pipe, Stream as S } from '@effect-ts/core'
 import { runMain } from '@effect-ts/node/Runtime'
 
 const program = T.gen(function* ($) {
+  yield* $(pipe(
+    S.fromEffect(T.die('oh no - defect in stream')),
+    S.mapErrorCause(err => {
+      console.log(`error in stream: ${err}`)
+      return err
+    }),
+    S.take(1),
+    S.runCollect,
+  ))
+
   yield* $(T.succeedWith(() => console.log('hello world')))
 })
 
